49222bc2fe0e714d5c77651913c743a55a27ff33
renato3x <pereirarenato21@gmail.com>
2024-10-28 17:07:48 -0300

    add: initial dependecies

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
+import { useFonts, Lato_400Regular } from '@expo-google-fonts/lato';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [ fontsLoaded ] = useFonts({
+    Lato_400Regular,
+  });
+
+  if (fontsLoaded) {
+    return (
+      <View style={styles.container}>
+        <StatusBar style="light" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
